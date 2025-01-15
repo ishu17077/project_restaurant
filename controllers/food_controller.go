a6331689c28c5b3f9f890b9ea9a913e47f489bb4
ishu17077 <ishu17077@users.noreply.github.com>
2025-01-15 12:02:26 +0530

    Minor changes

--- controllers/food_controller.go
+++ controllers/food_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"project_restaurant/database"
 	"project_restaurant/models"
@@ -22,22 +23,40 @@ var validate = validator.New()
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(c, 100*time.Second)
-		result, err := foodCollection.Find(context.TODO(), bson.M{})
+		// result, err := foodCollection.Find(context.TODO(), bson.M{})
 
 		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
 		if err != nil || recordPerPage < 1 {
 			recordPerPage = 10
 		}
 		page, err := strconv.Atoi(c.Query("page"))
-		if err != nil || page <1 {
+		if err != nil || page < 1 {
 			page = 1
 		}
-		startIndex := (page-1) * recordPerPage
+		startIndex := (page - 1) * recordPerPage
 		startIndex, err = strconv.Atoi(c.Query("startIndex"))
-
-		matchStage 
-		groupStage
-		projectStage
+		matchStage := bson.D{{Key: "$match", Value: bson.D{{}}}}
+		groupStage := bson.D{{Key: "$group", Value: bson.D{{Key: "_id", Value: bson.D{{Key: "_id", Value: "null"}}}, {Key: "total_count", Value: bson.D{{Key: "$sum", Value: 1}}}, {Key: "data", Value: bson.D{{Key: "$push", Value: "$$ROOT"}}}}}}
+		projectStage := bson.D{{
+			Key: "$project", Value: bson.D{
+				{Key: "_id", Value: 0},
+				{Key: "total_count", Value: 1},
+				{Key: "food_items", Value: bson.D{{Key: "$slice", Value: []interface{}{"$data", startIndex, recordPerPage}}}},
+			},
+		},
+		}
+		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage,
+		})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occued while listing food items."})
+		}
+		var allFoods []bson.M
+		if err = result.All(ctx, &allFoods); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allFoods[0])
 	}
 }
 
@@ -87,6 +106,7 @@ func CreateFood() gin.HandlerFunc {
 		if insertErr != nil {
 			msg := fmt.Sprintf("Food Item was not created")
 			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
 		}
 		defer cancel()
 		c.JSON(http.StatusOK, result)
@@ -95,7 +115,17 @@ func CreateFood() gin.HandlerFunc {
 }
 
 func UpdateFood() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, err = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+		var food models.Food
+		foodId = c.Param("food_id")
+
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
 }
 
 func round(num float64) int {
